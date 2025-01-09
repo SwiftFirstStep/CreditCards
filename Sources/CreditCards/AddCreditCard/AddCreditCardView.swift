ee36be0acdb5ee8bb18bfb8bc7319617a0b662a5
Vijay Pokuri <pokuri.vijay@gmail.com>
2025-01-09 08:53:22 +0530

    1

--- Sources/CreditCards/AddCreditCard/AddCreditCardView.swift
+++ Sources/CreditCards/AddCreditCard/AddCreditCardView.swift
@@ -16,214 +16,134 @@ public struct AddCreditCardView: View {
     @State private var selectedMonth = 1
     @State private var selectedYear = Calendar.current.component(.year, from: Date()) % 100
     
-    var months = Array(1...12)
-    var years = Array(24...34) // Example: 2024-2034, using 2-digit format
-    //
-
     public var body: some View {
         NavigationView {
             ScrollView(){
-            VStack(spacing: 20) {
-                
-                if !presenter.newCardNumber.isEmpty {
-                    CreditCardView(
-                        card: CreditCard(
-                            cardHolderName: presenter.newCardHolderName,
-                            cardNumber: presenter.newCardNumber,
-                            expiryDate: presenter.newExpiryDate,
-                            cvv: presenter.newCVV,
-                            cardType: presenter.cardType,
-                            gradientColors: presenter.gradientColors
+                VStack(spacing: 20) {
+                    
+                    if !presenter.newCardNumber.isEmpty {
+                        CreditCardView(
+                            card: CreditCard(
+                                cardHolderName: presenter.newCardHolderName,
+                                cardNumber: presenter.newCardNumber,
+                                expiryDate: presenter.newExpiryDate,
+                                cvv: presenter.newCVV,
+                                cardType: presenter.cardType,
+                                gradientColors: presenter.gradientColors
+                            )
                         )
-                    )
-                    .padding(.top)
-                }
-                
-                TextField("Card Number", text: $presenter.newCardNumber)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding(.horizontal)
-                    .padding(.top)
-                    .keyboardType(.numberPad)
-                    .onChange(of: presenter.newCardNumber) { value in
-                        var mutableValue = value
-                        mutableValue = mutableValue.filter { $0.isNumber || $0 == " " }
-                        
-                        // Ensure the input is no more than 16 digits and 3 spaces
-                        let digits = mutableValue.filter { $0.isNumber }
-                        let spaces = mutableValue.filter { $0 == " " }
-                        
-                        if digits.count > 16 {
-                            mutableValue = String(digits.prefix(16))
-                        }
-                        
-                        if spaces.count > 3 {
-                            let maxLength = digits.count + 3
-                            mutableValue = String(mutableValue.prefix(maxLength))
-                        }
-                        
-                        // Optionally auto-format the card number
-                        presenter.autoFormatCardNumber(&mutableValue)
-                        
-                        presenter.newCardNumber = mutableValue
-                        presenter.updateCardType(for: mutableValue)
+                        .padding(.top)
                     }
-                
-                ZStack {
-                    VStack {
-                        HStack {
-                            TextField("Expiry Date (MM/YY)", text: $presenter.newExpiryDate)
-                                .textFieldStyle(RoundedBorderTextFieldStyle())
-                                .keyboardType(.numbersAndPunctuation)
-                                .onChange(of: presenter.newExpiryDate) { value in
-                                    var mutableValue = value
-                                    mutableValue = mutableValue.filter { $0.isNumber || $0 == "/" }
-                                    if mutableValue.count > 2, !mutableValue.contains("/") {
-                                        mutableValue.insert("/", at: mutableValue.index(mutableValue.startIndex, offsetBy: 2))
-                                    }
-                                    
-                                    // Limit the input to MM/YY format
-                                    let components = mutableValue.split(separator: "/")
-
-                                    switch components.count {
-                                    case 2:
-                                        let month = components[0].prefix(2)
-                                        let year = components[1].prefix(2)
-                                        
-                                        // Validate the year (should be between 24 and 34)
-                                        if let yearInt = Int(year), yearInt < 24 && String(yearInt).count != 1 {
-                                                mutableValue = "\(month)/24" // Set to minimum year
-                                            } else if let yearInt = Int(year), yearInt > 34 {
-                                                mutableValue = "\(month)/34" // Set to maximum year
-                                            } else if let yearInt = Int(year), yearInt == 0 {
-                                                mutableValue = "\(month)/24" // Set to maximum year
-                                            }else {
-                                                mutableValue = "\(month)/\(year)"
-                                            }
-                                            
-                                            // Validate the month (should be between 1 and 12)
-                                            if let monthInt = Int(month), monthInt < 1 {
-                                                mutableValue = "01" // Set to minimum month
-                                            } else if let monthInt = Int(month), monthInt > 12 {
-                                                mutableValue = "12" // Set to maximum month
-                                            }
-                                        
-                                    case 1:
-                                        let month = components[0].prefix(2)
-                                        
-                                        // Validate the month and adjust if necessary
-                                        if let monthInt = Int(month), monthInt > months.last! {
-                                            mutableValue = "12"
-                                        } else if let monthInt = Int(month), monthInt == 0 {
-                                            mutableValue = "0"
-                                        } else if let monthInt = Int(month), monthInt < months.first! {
-                                            mutableValue = "1"
-                                        }
-                                        else {
-                                            mutableValue = "\(month)"
-                                        }
-                                        
-                                    case _ where components.count > 2:
-                                        let month = components[0].prefix(2)
-                                        let year = components[1].prefix(2)
-                                        mutableValue = "\(month)/\(year)"
-                                        
-                                    default:
-                                        break
-                                    }
-                                    // Assign the validated and formatted value
-                                    presenter.newExpiryDate = mutableValue
-                                }
-                            
-                            Button(action: {
-                                showPicker.toggle()
-                            }) {
-                                Image(systemName: "calendar")
-                                    .foregroundColor(.gray)
-                                    .padding(.trailing, 10)
-                            }
-                        }
-                        
-                        if showPicker {
-                            // Small custom view for the picker
-                            VStack {
-                                HStack {
-                                    Picker("Select Month", selection: $selectedMonth) {
-                                        ForEach(months, id: \.self) { month in
-                                            Text(String(format: "%02d", month)).tag(month)
-                                        }
-                                    }
-                                    .pickerStyle(WheelPickerStyle())
-                                    .frame(width: 150) // Control width of the month picker
-                                    
-                                    Picker("Select Year", selection: $selectedYear) {
-                                        ForEach(years, id: \.self) { year in
-                                            Text(String(format: "%02d", year)).tag(year)
-                                        }
-                                    }
-                                    .pickerStyle(WheelPickerStyle())
-                                    .frame(width: 150) // Control width of the year picker
-                                }
-                                
-                                Button("Done") {
-                                    presenter.newExpiryDate = String(format: "%02d / %02d", selectedMonth, selectedYear)
-                                    showPicker = false
-                                }
-                                .padding()
-                            }
-                            .frame(maxWidth: 320, maxHeight: 150)
-                            .padding(.trailing, 50)
-                            .background(Color(.systemGray6))
-                            .cornerRadius(10)
-                            .shadow(radius: 10)
-                        }
-                    }
-                    .padding(.horizontal)
-                }
-                
-                TextField("CVV", text: $presenter.newCVV)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding(.horizontal)
-                    .keyboardType(.numberPad)
-                    .onChange(of: presenter.newCVV) { value in
-                        presenter.newCVV = String(value.filter { $0.isNumber }.prefix(3))
-                    }
-                
-                TextField("Card Type", text: $presenter.cardType)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding(.horizontal)
-                    .disabled(true)
-                
-                TextField("Cardholder Name", text: $presenter.newCardHolderName)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding(.horizontal)
-                    .onChange(of: presenter.newCardHolderName) { value in
-                        // Limit the input to 51 characters
-                        presenter.newCardHolderName = String(value.prefix(21))
-                    }
-                
-                Button(action: {
-                    presenter.addCreditCard()
-                    showAddCardDrawer = false // Close the sheet after adding the card
-                }) {
-                    Text("Activate")
-                        .font(.headline)
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(presenter.isAddCardButtonEnabled ? Color.green : Color.gray)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
+                    
+                    TextField("Card Number", text: $presenter.newCardNumber)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
                         .padding(.horizontal)
+                        .padding(.top)
+                        .keyboardType(.numberPad)
+                        .onChange(of: presenter.newCardNumber) { value in
+                            presenter.updateNewCardNumber(value: value)
+                        }
+                    
+                    ZStack {
+                        VStack {
+                            HStack {
+                                TextField("Expiry Date (MM/YY)", text: $presenter.newExpiryDate)
+                                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                                    .keyboardType(.numbersAndPunctuation)
+                                    .onChange(of: presenter.newExpiryDate) { value in
+                                        presenter.updateNewExpiryDate(value: value)
+                                    }
+                                
+                                Button(action: {
+                                    showPicker.toggle()
+                                }) {
+                                    Image(systemName: "calendar")
+                                        .foregroundColor(.gray)
+                                        .padding(.trailing, 10)
+                                }
+                            }
+                            
+                            if showPicker {
+                                // Small custom view for the picker
+                                VStack {
+                                    HStack {
+                                        Picker("Select Month", selection: $selectedMonth) {
+                                            ForEach(presenter.months, id: \.self) { month in
+                                                Text(String(format: "%02d", month)).tag(month)
+                                            }
+                                        }
+                                        .pickerStyle(WheelPickerStyle())
+                                        .frame(width: 150) // Control width of the month picker
+                                        
+                                        Picker("Select Year", selection: $selectedYear) {
+                                            ForEach(presenter.years, id: \.self) { year in
+                                                Text(String(format: "%02d", year)).tag(year)
+                                            }
+                                        }
+                                        .pickerStyle(WheelPickerStyle())
+                                        .frame(width: 150) // Control width of the year picker
+                                    }
+                                    
+                                    Button("Done") {
+                                        presenter.newExpiryDate = String(format: "%02d / %02d", selectedMonth, selectedYear)
+                                        showPicker = false
+                                    }
+                                    .padding()
+                                }
+                                .frame(maxWidth: 320, maxHeight: 150)
+                                .padding(.trailing, 50)
+                                .background(Color(.systemGray6))
+                                .cornerRadius(10)
+                                .shadow(radius: 10)
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                    
+                    TextField("CVV", text: $presenter.newCVV)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.horizontal)
+                        .keyboardType(.numberPad)
+                        .onChange(of: presenter.newCVV) { value in
+                            presenter.newCVV = String(value.filter { $0.isNumber }.prefix(3))
+                        }
+                    
+                    TextField("Card Type", text: $presenter.cardType)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.horizontal)
+                        .disabled(true)
+                    
+                    TextField("Cardholder Name", text: $presenter.newCardHolderName)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.horizontal)
+                        .onChange(of: presenter.newCardHolderName) { value in
+                            // Limit the input to 51 characters
+                            presenter.newCardHolderName = String(value.prefix(21))
+                        }
+                    
+                    Button(action: {
+                        presenter.addCreditCard()
+                        showAddCardDrawer = false // Close the sheet after adding the card
+                    }) {
+                        Text("Activate")
+                            .font(.headline)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(presenter.isAddCardButtonEnabled ? Color.green : Color.gray)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .padding(.horizontal)
+                    }
+                    .disabled(!presenter.isAddCardButtonEnabled)
+                    Spacer()
                 }
-                .disabled(!presenter.isAddCardButtonEnabled)
-                Spacer()
+                .navigationTitle("Activate Credit Card")
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationBarItems(leading: Button("Cancel") {
+                    showAddCardDrawer = false // Close the sheet when canceling
+                } .foregroundColor(.white))
+                
             }
-            .navigationTitle("Activate Credit Card")
-            .navigationBarTitleDisplayMode(.inline)
-            .navigationBarItems(leading: Button("Cancel") {
-                showAddCardDrawer = false // Close the sheet when canceling
-            } .foregroundColor(.white))
-            
         }
-    }
     }
 }
--- Sources/CreditCards/CreditCardPresenter.swift
+++ Sources/CreditCards/CreditCardPresenter.swift
@@ -32,16 +32,19 @@ public class CreditCardPresenter: CreditCardPresenterProtocol {
     @Published public var cardType: String = ""
     @Published public var gradientColors = [Color.gray, Color.black]
     @Published private(set) public var isAddCardButtonEnabled: Bool = false
-
+    
+    var months = Array(1...12)
+    var years = Array(24...34)
+    
     private var interactor: CreditCardInteractorProtocol
     private var cancellables = Set<AnyCancellable>()
-
+    
     public init(interactor: CreditCardInteractorProtocol) {
         self.interactor = interactor
         setupBindings()
         creditCards = interactor.fetchCreditCards()
     }
-
+    
     private func setupBindings() {
         // Combine the latest values from the published properties
         Publishers.CombineLatest4($newCardHolderName, $newCardNumber, $newExpiryDate, $newCVV)
@@ -54,10 +57,7 @@ public class CreditCardPresenter: CreditCardPresenterProtocol {
             }
             .assign(to: &$isAddCardButtonEnabled)
     }
-
-
-
-
+    
     public func addCreditCard() {
         let gradientColors: [Color]
         switch cardType {
@@ -72,7 +72,7 @@ public class CreditCardPresenter: CreditCardPresenterProtocol {
         default:
             gradientColors = [Color.gray, Color.black]
         }
-
+        
         let newCard = CreditCard(
             cardHolderName: newCardHolderName,
             cardNumber: newCardNumber,
@@ -81,14 +81,13 @@ public class CreditCardPresenter: CreditCardPresenterProtocol {
             cardType: cardType,
             gradientColors: gradientColors
         )
-
+        
         creditCards.append(newCard)
         clearForm()
         isAddCardButtonEnabled = false // Disable the button after adding a card
         NotificationCenter.default.post(name: .dismissAddCardView, object: nil)
     }
-
-
+    
     public func updateCardType(for number: String) {
         let prefix = String(number.prefix(4).replacingOccurrences(of: " ", with: ""))
         switch prefix {
@@ -113,7 +112,7 @@ public class CreditCardPresenter: CreditCardPresenterProtocol {
             gradientColors = [Color.gray, Color.black]
         }
     }
-
+    
     public func autoFormatCardNumber(_ number: inout String) {
         number = number.replacingOccurrences(of: "[^0-9]", with: "", options: .regularExpression)
         number = stride(from: 0, to: number.count, by: 4).map {
@@ -122,14 +121,98 @@ public class CreditCardPresenter: CreditCardPresenterProtocol {
             return String(number[start..<end])
         }.joined(separator: " ")
     }
-
+    
     public func autoFormatExpiryDate(_ expiry: inout String) {
         expiry = expiry.replacingOccurrences(of: "[^0-9]", with: "", options: .regularExpression)
         if expiry.count > 2 {
             expiry.insert("/", at: expiry.index(expiry.startIndex, offsetBy: 2))
         }
     }
-
+    
+    public func updateNewExpiryDate(value: String){
+        var mutableValue = value
+        mutableValue = mutableValue.filter { $0.isNumber || $0 == "/" }
+        if mutableValue.count > 2, !mutableValue.contains("/") {
+            mutableValue.insert("/", at: mutableValue.index(mutableValue.startIndex, offsetBy: 2))
+        }
+        
+        // Limit the input to MM/YY format
+        let components = mutableValue.split(separator: "/")
+        
+        switch components.count {
+        case 2:
+            let month = components[0].prefix(2)
+            let year = components[1].prefix(2)
+            
+            // Validate the year (should be between 24 and 34)
+            if let yearInt = Int(year), yearInt < 24 && String(yearInt).count != 1 {
+                mutableValue = "\(month)/24" // Set to minimum year
+            } else if let yearInt = Int(year), yearInt > 34 {
+                mutableValue = "\(month)/34" // Set to maximum year
+            } else if let yearInt = Int(year), yearInt == 0 {
+                mutableValue = "\(month)/24" // Set to maximum year
+            }else {
+                mutableValue = "\(month)/\(year)"
+            }
+            
+            // Validate the month (should be between 1 and 12)
+            if let monthInt = Int(month), monthInt < 1 {
+                mutableValue = "01" // Set to minimum month
+            } else if let monthInt = Int(month), monthInt > 12 {
+                mutableValue = "12" // Set to maximum month
+            }
+            
+        case 1:
+            let month = components[0].prefix(2)
+            
+            // Validate the month and adjust if necessary
+            if let monthInt = Int(month), monthInt > months.last! {
+                mutableValue = "12"
+            } else if let monthInt = Int(month), monthInt == 0 {
+                mutableValue = "0"
+            } else if let monthInt = Int(month), monthInt < months.first! {
+                mutableValue = "1"
+            }
+            else {
+                mutableValue = "\(month)"
+            }
+            
+        case _ where components.count > 2:
+            let month = components[0].prefix(2)
+            let year = components[1].prefix(2)
+            mutableValue = "\(month)/\(year)"
+            
+        default:
+            break
+        }
+        // Assign the validated and formatted value
+        newExpiryDate = mutableValue
+    }
+    
+    public func updateNewCardNumber(value: String){
+        var mutableValue = value
+        mutableValue = mutableValue.filter { $0.isNumber || $0 == " " }
+        
+        // Ensure the input is no more than 16 digits and 3 spaces
+        let digits = mutableValue.filter { $0.isNumber }
+        let spaces = mutableValue.filter { $0 == " " }
+        
+        if digits.count > 16 {
+            mutableValue = String(digits.prefix(16))
+        }
+        
+        if spaces.count > 3 {
+            let maxLength = digits.count + 3
+            mutableValue = String(mutableValue.prefix(maxLength))
+        }
+        
+        // Optionally auto-format the card number
+        autoFormatCardNumber(&mutableValue)
+        
+        newCardNumber = mutableValue
+        updateCardType(for: mutableValue)
+    }
+    
     private func clearForm() {
         newCardHolderName = ""
         newCardNumber = ""
